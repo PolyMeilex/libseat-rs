acea52680171878b16e5b41f165c736cf6dcb10f
Poly <marynczak.bartlomiej@gmail.com>
2021-06-11 01:17:00 +0200

    Cleanup sys crate

--- libseat-sys/build.rs
+++ libseat-sys/build.rs
@@ -8,6 +8,15 @@ fn main() {
     let bindings = Builder::default()
         .header("src/wrapper.h")
         .layout_tests(false)
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .allowlist_recursively(false)
+        .allowlist_type("libseat.*")
+        .allowlist_function("libseat.*")
+        .allowlist_var("libseat.*")
+        .blocklist_item("libseat_set_log_handler")
+        .blocklist_item("libseat_log_func")
         .generate()
         .expect("Unable to generate bindings");
 
