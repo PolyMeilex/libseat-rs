c0788d3271bf334d77a873641022e5331c0656eb
PolyMeilex <marynczakbartlomiej@gmail.com>
2023-07-03 19:58:56 +0200

    Remove slog

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,33 +2,25 @@ use libseat::{Seat, SeatEvent};
 
 use std::{cell::RefCell, rc::Rc};
 
-use slog::*;
-use std::sync::Mutex;
-
 fn main() {
-    let logger = slog::Logger::root(Mutex::new(slog_term::term_full().fuse()).fuse(), o!());
-
     let active = Rc::new(RefCell::new(false));
 
     let seat = {
         let active = active.clone();
-        Seat::open(
-            move |seat, event| match event {
-                SeatEvent::Enable => {
-                    println!("Enable");
-                    println!("Name: {}", seat.name());
+        Seat::open(move |seat, event| match event {
+            SeatEvent::Enable => {
+                println!("Enable");
+                println!("Name: {}", seat.name());
 
-                    *active.borrow_mut() = true;
-                }
-                SeatEvent::Disable => {
-                    println!("Disable");
+                *active.borrow_mut() = true;
+            }
+            SeatEvent::Disable => {
+                println!("Disable");
 
-                    *active.borrow_mut() = false;
-                    seat.disable().unwrap();
-                }
-            },
-            logger,
-        )
+                *active.borrow_mut() = false;
+                seat.disable().unwrap();
+            }
+        })
     };
 
     if let Ok(mut seat) = seat {
