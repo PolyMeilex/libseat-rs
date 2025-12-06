61421ddeaba3d849bc09462ea4b6c449947b9693
Tomasz Kramkowski <tomasz@kramkow.ski>
2025-12-06 21:19:09 +0000

    Remove `LogHandler` entirely
    Since the singleton doesn't expose any functions, doesn't implement drop
    and doesn't hold any data, it doesn't need to be a singleton.
    
    Use `Once` to ensure log setup happens only once.

--- src/log_handler.rs
+++ src/log_handler.rs
@@ -1,5 +1,5 @@
 use log::{debug, error, info, trace};
-use std::{ffi::CStr, os::raw::c_char, sync::LazyLock};
+use std::{ffi::CStr, os::raw::c_char, sync::Once};
 
 use crate::LogLevel;
 
@@ -9,40 +9,30 @@ extern "C" {
     fn init_preformatted_log_handler(handler: LogHandlerFn);
 }
 
-/// Custom LibSeat log handler
-#[derive(Debug)]
-struct LogHandler;
-
-impl LogHandler {
-    fn new() -> Self {
-        crate::set_log_level(LogLevel::Debug);
-
-        unsafe { init_preformatted_log_handler(ffi_handler) };
-
-        Self
-    }
-
-    fn log(level: LogLevel, msg: &str) {
-        match level {
-            LogLevel::Silent => trace!("{}", msg),
-            LogLevel::Error => error!("{}", msg),
-            LogLevel::Info => info!("{}", msg),
-            LogLevel::Debug | LogLevel::Last => debug!("{}", msg),
-        }
+fn log(level: LogLevel, msg: &str) {
+    match level {
+        LogLevel::Silent => trace!("{}", msg),
+        LogLevel::Error => error!("{}", msg),
+        LogLevel::Info => info!("{}", msg),
+        LogLevel::Debug | LogLevel::Last => debug!("{}", msg),
     }
 }
 
 extern "C" fn ffi_handler(level: LogLevel, msg: *const c_char) {
     let cstr = unsafe { CStr::from_ptr(msg) };
     match cstr.to_str() {
-        Ok(msg) => LogHandler::log(level, msg),
+        Ok(msg) => log(level, msg),
         Err(err) => error!("{:?}", err),
     }
 }
 
-static LOG_HANDLER: LazyLock<LogHandler> = LazyLock::new(LogHandler::new);
+static INIT: Once = Once::new();
 
 /// Initialise libseat log handler
 pub fn init() {
-    let _ = &*LOG_HANDLER;
+    INIT.call_once(|| {
+        crate::set_log_level(LogLevel::Debug);
+
+        unsafe { init_preformatted_log_handler(ffi_handler) };
+    })
 }
